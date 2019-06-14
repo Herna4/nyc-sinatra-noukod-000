4c031dc5252929a4dbaad65919a0fd91298b26b0
Herna Michel <labellehernamichel@yahoo.fr>
2019-06-14 16:09:41 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,4 +1,10 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  set :views, proc { File.join(root, '../views/') }
+  register Sinatra::Twitter::Bootstrap::Assets
 
+  get '/figures' do
+    @figures = Figure.all
+    erb :"figures/index"
+  end
 end
