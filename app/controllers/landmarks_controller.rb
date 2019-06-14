b4c56e77ce11c0d6addf866b49c34059e0b90376
Herna Michel <labellehernamichel@yahoo.fr>
2019-06-14 16:16:11 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,10 +1,16 @@
 class LandmarksController < ApplicationController
   # add controller methods
-  set :views, proc { File.join(root, '../views/') }
-  register Sinatra::Twitter::Bootstrap::Assets
-
   get '/landmarks' do
-    @landmarks = Landmarks.all
+    @landmarks = Landmark.all
     erb :"landmarks/index"
+  end
+
+  get '/landmarks/new' do
+    erb :"landmarks/new"
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    redirect to "/landmarks/#{@landmark.id}"
   end
 end
